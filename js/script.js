419bed02a99d40f3dea14adfe938cbef414e5f4d
DHRUV-KILLER <69303943+DHRUV-KILLER@users.noreply.github.com>
2022-04-05 16:25:23 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -322,3 +322,69 @@ $( document ).ready(function() {
 	});
   });
 
+
+ /* MODAL MAIN */ 
+ 		// Get the modal
+		 var modal = document.getElementById("myModal");
+		
+		 // Get the button that opens the modal
+		 var btn = document.getElementById("myBtn");
+		 
+		 // Get the <span> element that closes the modal
+		 var span = document.getElementsByClassName("close")[0];
+		 
+		 // When the user clicks the button, open the modal 
+		 btn.onclick = function() {
+		   modal.style.display = "block";
+		 }
+		 
+		 // When the user clicks on <span> (x), close the modal
+		 span.onclick = function() {
+		   modal.style.display = "none";
+		 }
+		 
+		 // When the user clicks anywhere outside of the modal, close it
+		 window.onclick = function(event) {
+		   if (event.target == modal) {
+			 modal.style.display = "none";
+		   }
+		 }
+
+
+/* back to top new */
+	//Get the button
+	let mybutton = document.getElementById("btn-back-to-top");
+	
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+	  scrollFunction();
+	};
+	
+	function scrollFunction() {
+	  if (
+		document.body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	  ) {
+		mybutton.style.display = "block";
+	  } else {
+		mybutton.style.display = "none";
+	  }
+	}
+	// When the user clicks on the button, scroll to the top of the document
+	mybutton.addEventListener("click", backToTop);
+	
+	function backToTop() {
+	  document.body.scrollTop = 0;
+	  $("html, body").animate({ scrollTop: 0 }, 200); 
+	}
+
+
+
+/* collapsable header or accordian */
+let collapsibleHeaders = document.getElementsByClassName("collapsible__header");
+
+Array.from(collapsibleHeaders).forEach((header) => {
+  header.addEventListener("click", () => {
+    header.parentElement.classList.toggle("collapsible--open");
+  });
+});
